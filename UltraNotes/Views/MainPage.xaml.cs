dd516ea12de3af2a0307388aa323bd10a1a4cabd
jpb <jakub.baranski.33@gmail.com>
2023-12-03 19:50:50 +0000

    Add basic functionality

--- UltraNotes/Views/MainPage.xaml.cs
+++ UltraNotes/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 using UltraNotes.ViewModels;
 
@@ -15,5 +17,45 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private void BoldButton_Click(object sender, RoutedEventArgs e)
+    {
+        ITextSelection selectedText = editor.Document.Selection;
+        if (selectedText != null)
+        {
+            ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+            charFormatting.Bold = FormatEffect.Toggle;
+            selectedText.CharacterFormat = charFormatting;
+        }
+    }
+
+    private void ItalicButton_Click(object sender, RoutedEventArgs e)
+    {
+        ITextSelection selectedText = editor.Document.Selection;
+        if (selectedText != null)
+        {
+            ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+            charFormatting.Italic = FormatEffect.Toggle;
+            selectedText.CharacterFormat = charFormatting;
+        }
+    }
+
+    private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+    {
+        ITextSelection selectedText = editor.Document.Selection;
+        if (selectedText != null)
+        {
+            ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+            if (charFormatting.Underline == UnderlineType.None)
+            {
+                charFormatting.Underline = UnderlineType.Single;
+            }
+            else
+            {
+                charFormatting.Underline = UnderlineType.None;
+            }
+            selectedText.CharacterFormat = charFormatting;
+        }
     }
 }
